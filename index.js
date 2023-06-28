d0f3189b2937df45aa2da04a12709812a298bef1
Amiodas <amio.das.dip@gmail.com>
2023-06-29 00:31:23 +0100

    #create booking collection, #add bookings, #delete bookings, #find bookings id wise

--- index.js
+++ index.js
@@ -25,6 +25,7 @@ async function run() {
     await client.connect();
 
     const serviceCollection = client.db("carDoctorsDB").collection("services");
+    const bookingCollection = client.db("carDoctorsDB").collection("bookings");
 
     app.get("/services", async (req, res) => {
       const cursor = serviceCollection.find();
@@ -35,7 +36,35 @@ async function run() {
     app.get("/services/:id", async (req, res) => {
       const id = req.params.id;
       const query = { _id: new ObjectId(id) };
-      const result = await serviceCollection.findOne(query);
+      const options = {
+        projection: { title: 1, img: 1, price: 1, service_id: 1 },
+      };
+      const result = await serviceCollection.findOne(query, options);
+      res.send(result);
+    });
+
+    // bookings
+    app.post("/bookings", async (req, res) => {
+      const booking = req.body;
+      console.log(booking);
+      const result = await bookingCollection.insertOne(booking);
+      res.send(result);
+    });
+
+    app.get("/bookings", async (req, res) => {
+      let query = {};
+      if (req.query?.email) {
+        query = { email: req.query.email };
+      }
+      const result = await bookingCollection.find(query).toArray();
+      res.send(result);
+    });
+
+    app.delete("/bookings/:id", async (req, res) => {
+      const id = req.params.id;
+      console.log(id);
+      const query = { _id: new ObjectId(id) };
+      const result = await bookingCollection.deleteOne(query);
       res.send(result);
     });
 
